dec9770c340e76b9c863013ee796219539a53cd4
fitmeat <eenot825@gmail.com>
2022-11-22 11:25:33 +0300

    Выполнил задание 8

---
+++ task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+for(int i = 1; i<=N; i++){
+    if(i%2==0){
+        Console.Write($"{i} ");
+    }
+}
