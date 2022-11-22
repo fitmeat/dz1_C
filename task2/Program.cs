803c400ea0e586b494ffcdc3d58f7ef132f4bd23
fitmeat <eenot825@gmail.com>
2022-11-22 11:06:10 +0300

    Выполнил задание 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if(a>b){
+    Console.WriteLine("max = " + a);
+} else Console.WriteLine("max = " + b);
