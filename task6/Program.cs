8a5a749d7fa4e5426dc3859e37fbf13f644e7965
fitmeat <eenot825@gmail.com>
2022-11-22 11:20:26 +0300

    Выполнил задание 6

---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int k = int.Parse(Console.ReadLine());
+if(k%2==0){
+    Console.WriteLine("Четное");
+} else Console.WriteLine("Нечетное");
